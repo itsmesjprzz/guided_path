2de24130745008b90f3f1362b01b2bbe49d1f137
itsmesjprzz <46174358+itsmesjprzz@users.noreply.github.com>
2026-05-03 16:47:57 +0800

    fix brevo smtp port

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ app.secret_key = "secret"
 app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER", "smtp-relay.brevo.com")
 app.config["MAIL_PORT"] = int(os.environ.get("MAIL_PORT", 587))
 app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 
